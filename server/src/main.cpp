cfb340c9a1872c8f5f6e26afa3bd0c8613b13475
Preciado <aarongarpre@gmail.com>
2023-07-06 12:36:45 +0200

    Started with project

---
+++ server/src/main.cpp
@@ -0,0 +1,14 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <iostream>
+#include <Windows.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+int main() {
+
+
+	return 0;
+}
