0e189d66488a66fbc6713d71070b768d508bb4e2
Preciado <aarongarpre@gmail.com>
2023-07-06 13:48:17 +0200

    Started with our Listen thread

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -6,18 +6,37 @@
 #include <Windows.h>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
+#include <thread>
+#include <mutex>
+#include <map>
+
+void ListenThread();
+
+std::map<std::string, SOCKET> clients;
+std::mutex clientsMutex;
 
 int main() {
 	WSADATA wsa;
 	int nRes = WSAStartup(MAKEWORD(2, 2), &wsa);
 
 	if (nRes != 0) {
-		std::cout << "[ERROR] An error occured during WSAStartup." << std::endl;
+		std::cout << "[ERROR] An error occurred during WSAStartup." << std::endl;
 		throw std::exception();
 		return 1;
 	}
 
-
+	std::thread listenThread(ListenThread);
+	listenThread.detach();
 
 	return 0;
+}
+
+void ListenThread() {
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (sock == INVALID_SOCKET) {
+		std::cout << "[ERROR] An error occurred with our server socket initialization" << std::endl;
+		throw std::exception();
+		return;
+	}
 }
