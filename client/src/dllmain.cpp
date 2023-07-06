9dbb76c7cc3c483868ffd27798c7bd98ec4ca477
Preciado <aarongarpre@gmail.com>
2023-07-06 23:49:20 +0200

    Started with MarlborgeDLL

--- client/src/dllmain.cpp
+++ client/src/dllmain.cpp
@@ -1,10 +1,28 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
 #include <iostream>
 #include <Windows.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+void DebugMode();
 
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpvReserved) {
 	switch (dwReason) {
 	case DLL_PROCESS_ATTACH:
+#ifndef NDEBUG
+		DebugMode();
+#endif
 		break;
 	}
 	return TRUE;
+}
+
+void DebugMode() {
+	FILE* f;
+	AllocConsole();
+	freopen_s(&f, "CONOUT$", "w", stdout);
+	return;
 }
