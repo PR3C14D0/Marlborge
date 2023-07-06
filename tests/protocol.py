0935bcdcd0a3181e8a9ea957327779f7bc12bc29
Preciado <aarongarpre@gmail.com>
2023-07-06 17:35:58 +0200

    Started with protocol tests on python

---
+++ tests/protocol.py
@@ -0,0 +1,15 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect(('localhost', 5000))
+s.send(bytes([0x01]))
+
+data = s.recv(1024)
+if len(data) > 0 and data[0] == 0x01:
+	code = data[1:].decode()
+	print("Received: " + code)
+	response = bytes([0x01]) + code.encode()
+	s.send(response)
+
+s.close()
