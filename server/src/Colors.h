a0713818ab6551d4e37fe3d893eecfec985db88c
Preciado <aarongarpre@gmail.com>
2024-03-09 14:24:42 +0100

    Created: Colors.h

---
+++ server/src/Colors.h
@@ -0,0 +1,26 @@
+#pragma once
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+
+#define BOLD    "\033[1m"       /* Bold */
+#define UNDERLINE "\033[4m"     /* Underline */
+#define BLINK   "\033[5m"       /* Blink */
+#define REVERSE "\033[7m"       /* Reverse video */
+#define CONCEALED "\033[8m"     /* Concealed */
+
+// Background colors
+#define BGBLACK   "\033[40m"    /* Black */
+#define BGRED     "\033[41m"    /* Red */
+#define BGGREEN   "\033[42m"    /* Green */
+#define BGYELLOW  "\033[43m"    /* Yellow */
+#define BGBLUE    "\033[44m"    /* Blue */
+#define BGMAGENTA "\033[45m"    /* Magenta */
+#define BGCYAN    "\033[46m"    /* Cyan */
+#define BGWHITE   "\033[47m"    /* White */
