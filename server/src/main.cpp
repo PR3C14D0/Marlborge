d6035f113ad3af77bea8fe5ef5ff51ccc59948df
Preciado <aarongarpre@gmail.com>
2024-03-09 14:55:39 +0100

    Server now prints out Marlborge on console and started with main loop

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,10 +1,31 @@
-#include <iostream>
+﻿#include <iostream>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 #include <Windows.h>
 #include <thread>
 #include <mutex>
+#include "Colors.h"
+
+bool g_bQuit = false;
 
 int main() {
+	std::cout << RED << R"(                                                              
+,--.   ,--.               ,--.,--.                                
+|   `.'   | ,--,--.,--.--.|  ||  |-.  ,---. ,--.--. ,---.  ,---.  
+|  |'.'|  |' ,-.  ||  .--'|  || .-. '| .-. ||  .--'| .-. || .-. : 
+|  |   |  |\ '-'  ||  |   |  || `-' |' '-' '|  |   ' '-' '\   --. 
+`--'   `--' `--`--'`--'   `--' `---'  `---' `--'   .`-  /  `----' 
+                                                   `---'          )" << std::endl;
+	std::cout << BLUE << "Made for educational purposes only." << std::endl;
+	std::cout << WHITE << "Version: " << GREEN << "Freshman (0.1)" << std::endl;
+	std::cout << WHITE << "By: " << RED << "Preciado" << std::endl << std::endl;
+
+	while (!g_bQuit) {
+		std::cout << WHITE << "preciado@marlborge:~$ ";
+
+		std::string input;
+		std::getline(std::cin, input);
+	}
+
 	return 0;
 }
