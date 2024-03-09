65333676b3eb0dd134bdbb1ea22be6774044be45
Preciado <aarongarpre@gmail.com>
2024-03-09 15:53:54 +0100

    Started with our Listen thread

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -5,10 +5,22 @@
 #include <thread>
 #include <mutex>
 #include "Colors.h"
+#include "Util.h"
 
 bool g_bQuit = false;
 
+enum COMMANDS {
+	DDOS = 0x01
+};
+
+void ListenThread();
+
 int main() {
+#ifdef WIN32
+	WSADATA wsa;
+	WSAStartup(MAKEWORD(2, 2), &wsa);
+#endif
+
 	std::cout << RED << R"(                                                              
 ,--.   ,--.               ,--.,--.                                
 |   `.'   | ,--,--.,--.--.|  ||  |-.  ,---. ,--.--. ,---.  ,---.  
@@ -20,6 +32,9 @@ int main() {
 	std::cout << WHITE << "Version: " << GREEN << "Freshman (0.1)" << std::endl;
 	std::cout << WHITE << "By: " << RED << "Preciado" << std::endl << std::endl;
 
+	std::thread listenThread(ListenThread);
+	listenThread.detach();
+
 	while (!g_bQuit) {
 		std::cout << WHITE << "preciado@marlborge:~$ ";
 
@@ -28,4 +43,8 @@ int main() {
 	}
 
 	return 0;
+}
+
+void ListenThread() {
+
 }
